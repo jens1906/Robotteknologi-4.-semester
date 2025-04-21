01df4fcc2ed4646e23a7cb7c071f5f13209299c1
Jens Søby Hansen <jens1906@gmail.com>
2025-04-21 17:09:40 +0200

    REMOVE: Removed combiled C

--- ViconUDPConnection/ViconUDPRecieverAndRosPublisherCpp/build/my_cpp_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jens/Documents/GitHub/Robotteknologi-4.-semester/ViconUDPConnection/ViconUDPRecieverAndRosPublisherCpp/src/my_cpp_pkg
-# Build directory: /home/jens/Documents/GitHub/Robotteknologi-4.-semester/ViconUDPConnection/ViconUDPRecieverAndRosPublisherCpp/build/my_cpp_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
