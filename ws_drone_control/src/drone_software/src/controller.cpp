4ed69b211f239c5eef89669ac583e1cea5125c27
DjangoLan <thor2617@gmail.com>
2025-04-22 14:01:24 +0200

    Updated stuff

--- ws_drone_control/src/drone_software/src/controller.cpp
+++ ws_drone_control/src/drone_software/src/controller.cpp
@@ -30,7 +30,7 @@ void Controller::initialize(rclcpp::Node::SharedPtr node) {
                 vicon_position_[4] = msg->data[6];
                 vicon_position_[5] = msg->data[7];
                 RCLCPP_INFO(rclcpp::get_logger("offboard_control_node"),
-                            "Vicon update: x=%.2f, y=%.2f, z=%.2f", vicon_position_[0], vicon_position_[1], vicon_position_[2]);
+                            "Vicon update: x=%.2f, y=%.2f, z=%.2f", vicon_position_[2], vicon_position_[3], vicon_position_[4]);
             }
         });
 }
