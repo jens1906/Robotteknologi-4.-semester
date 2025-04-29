3e4834f6b2e44fb50ce3efae53fc5b88164e06fa
André Vester Magnusson <77392011+Andr465m@users.noreply.github.com>
2025-04-29 10:27:06 +0200

    Revert "a test site"
    This reverts commit af9685e036a729f7c110a9c622a22d2192069f8e.

--- wsa/src/testws/src/main.cpp
+++ wsa/src/testws/src/main.cpp
@@ -57,11 +57,7 @@ private:
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
             attitude_msg.thrust_body = {0.0, 0.0, -0.5};  // Thrust of 0.5
-=======
-            attitude_msg.thrust_body = {0.0, 0.0, -0.1};  // Thrust of 0.5
->>>>>>> Stashed changes
 =======
             attitude_msg.thrust_body = {0.0, 0.0, -0.1};  // Thrust of 0.5
 >>>>>>> Stashed changes
