858c7cee05c9183caf785cbbcae14c22e6eeda8a
André Vester Magnusson <77392011+Andr465m@users.noreply.github.com>
2025-04-29 10:21:10 +0200

    a test site

--- wsa/src/testws/src/main.cpp
+++ wsa/src/testws/src/main.cpp
@@ -55,7 +55,11 @@ private:
             attitude_msg.q_d = {1.0, 0.0, 0.0, 0.0};  // Quaternion: no rotation
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             attitude_msg.thrust_body = {0.0, 0.0, -0.5};  // Thrust of 0.5
+=======
+            attitude_msg.thrust_body = {0.0, 0.0, -0.1};  // Thrust of 0.5
+>>>>>>> Stashed changes
 =======
             attitude_msg.thrust_body = {0.0, 0.0, -0.1};  // Thrust of 0.5
 >>>>>>> Stashed changes
