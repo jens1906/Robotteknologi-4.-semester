172ae7b55c7d766a488bbff08b2ab9d7390f1484
André Vester Magnusson <77392011+Andr465m@users.noreply.github.com>
2025-05-02 12:57:33 +0200

    Adds landing position initialization
    Initializes landing_position with default values to ensure proper loop logic and prevent uninitialized variable issues.

--- ws_drone_control/src/drone_software/src/main.cpp
+++ ws_drone_control/src/drone_software/src/main.cpp
@@ -78,6 +78,7 @@ int main(int argc, char *argv[]) {
                 controller.vicon_position_[1],
                 controller.goal_position[2]
             };
+            landing_position = {100,100};
             while(std::abs(landing_position[1] - landing_position[0]) > 0.05f || landing_position[1] == 100) {
                 auto current_time = std::chrono::steady_clock::now();
                 auto elapsed_time = std::chrono::duration_cast<std::chrono::seconds>(current_time - last_execution_time);
