60dd802095bd72be37ea654c458a0461118bd256
MazzMT <mads.majlund.thomsen@gmail.com>
2025-05-08 15:27:49 +0200

    Added Integral for inner XY-control

--- ws_drone_control/src/drone_software/src/controller.hpp
+++ ws_drone_control/src/drone_software/src/controller.hpp
@@ -72,6 +72,7 @@ private:
     float pi_ = 3.14159265358979323846; // Pi constant
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     float Kp_xy_outer = 2.0f;    // 0.111f; //Måske prøve 0.9804f   ?
     float Kd_xy_outer = 0.2f;    // 0.6128f; //0.1804f; //Måske prøve 0.6128f   ?
     float Kp_xy_inner = 0.2;     // 0.2976; //0.1f; //Måske prøve 0.2788f   ?
@@ -79,6 +80,8 @@ private:
     float Kp_z = 1.3f;           // Stable p 1.3f | 0.8173f Settling time 10s | Perhaps 0.20430f (Settling time 20s)
     float Kd_z = 1.0f;           // Stable d 1.0f | 2.2140f Settling time 10s | Perhaps 1.10700f (Settling time 20s)
 =======
+=======
+>>>>>>> Stashed changes
     float x_integral_inner_ = 0.0f; // Integral term for x inner PID controller
     float y_integral_inner_ = 0.0f; // Integral term for y inner PID controller
 
